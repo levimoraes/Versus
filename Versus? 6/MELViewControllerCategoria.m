ddfc64a53b045efe2c6b600f0b7e4807be65b57d
Eduardo <edu-23@hotmail.com>
2014-08-28 15:28:13 -0300

    plist random

--- Versus? 6/MELViewControllerCategoria.m
+++ Versus? 6/MELViewControllerCategoria.m
@@ -12,16 +12,51 @@
 #import "MELViewControllerHerois.h"
 
 @interface MELViewControllerCategoria ()
-
+@property (nonatomic) NSMutableArray *ordemPerg;
 @end
 
 @implementation MELViewControllerCategoria
+-(NSString*)caminhoHerois
+{
+    NSString *pathStr = [[NSBundle mainBundle] bundlePath];
+    NSString *caminho = [pathStr stringByAppendingPathComponent:@"herois.plist"];
+    return caminho;
+}
+
+-(NSString*)caminhoOrdem
+{
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains
+    (NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    NSString *fileName = [NSString stringWithFormat:@"%@/ordem.plist",documentsDirectory];
+    
+    return fileName;
+}
+
+-(void)sortearHerois{
+    NSMutableArray *perguntas = [[NSMutableArray alloc]initWithContentsOfFile:[self caminhoHerois]];
+    
+    _ordemPerg = [[NSMutableArray alloc]init];
+    while (_ordemPerg.count < perguntas.count) {
+        int randomNumber = 1 + arc4random() % perguntas.count;
+        if (![_ordemPerg containsObject:[NSString stringWithFormat:@"%d",randomNumber]])       {
+            [_ordemPerg addObject:[NSString stringWithFormat:@"%d",randomNumber]];
+        }
+        continue;
+    }
+    
+    [_ordemPerg writeToFile:[self caminhoOrdem] atomically:YES];
+}
+
 - (IBAction)BotaoVilao:(id)sender {
     MELViewControllerViloes *vilao = [[MELViewControllerViloes alloc]init];
     [self presentViewController:vilao animated:YES completion:nil];
 }
 - (IBAction)BotaoHeroi:(id)sender {
     MELViewControllerHerois *heroi = [[MELViewControllerHerois alloc]init];
+    [self sortearHerois];
     [self presentViewController:heroi animated:YES completion:nil];
 }
 - (IBAction)BotaoVoltar:(id)sender {
